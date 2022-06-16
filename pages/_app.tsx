8706594145e4268811602e5497124cb5374bf950
Heonyoung Kim <altenull@gmail.com>
2022-06-16 22:27:30 +0900

    build: Install swr module and configure it globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { SWRConfig } from 'swr';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <SWRConfig
+          value={{
+            refreshInterval: 0,
+            fetcher: (url: string) => fetch(url).then((res) => res.json()),
+          }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
       </ChakraProvider>
     </RecoilRoot>
   );
