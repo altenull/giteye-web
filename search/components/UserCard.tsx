5bb263f0294d0609bd3646aefa4f5abe623370b0
Heonyoung Kim <altenull@gmail.com>
2022-07-04 22:01:54 +0900

    WIP; New UserCard UI

--- search/components/UserCard.tsx
+++ search/components/UserCard.tsx
@@ -1,3 +1,4 @@
+import { EmailIcon } from '@chakra-ui/icons';
 import { Box, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import Image from 'next/image';
@@ -18,59 +19,96 @@ const UserCard: React.FC<Props> = React.memo(({ searchUser }) => {
     <Box
       position="relative"
       cursor="pointer"
-      display="flex"
-      alignItems="center"
-      maxW="xl"
-      borderWidth="2px"
-      borderRadius="lg"
-      overflow="hidden"
+      marginBottom="40px"
       onClick={() => navigateToRepositories(searchUser.login)}
     >
-      <StyledBlurredAvatarBackground
+      <StyledBlurredBackCover
+        maxW="xl"
         style={{
           backgroundImage: `url(${searchUser.avatar_url})`,
         }}
       />
 
-      <Image
-        style={{
-          borderRadius: '50%',
-        }}
-        src={searchUser.avatar_url}
-        alt={searchUser.login}
-        width={72}
-        height={72}
-      />
+      <Box
+        position="relative"
+        display="flex"
+        alignItems="center"
+        maxW="xl"
+        padding="16px"
+        border="1px solid #000000"
+        backgroundColor="white"
+      >
+        <StyledUsername>
+          <span>{searchUser.login}</span>
+          {typeof user?.name === 'string' && <span>({user.name})</span>}
+        </StyledUsername>
 
-      <Text style={{ marginLeft: '12px' }} fontSize="xl">
-        {searchUser.login}
-      </Text>
+        <StyledAvatar
+          style={{
+            position: 'absolute',
+            right: 0,
+            top: 0,
+            border: '1px solid #000000',
+          }}
+          src={searchUser.avatar_url}
+          alt={searchUser.login}
+          width={72}
+          height={72}
+        />
 
-      <Box>
-        <Text fontSize="md">followers_total: {user?.followers_total ?? '-'}</Text>
-        <Text fontSize="md">following_total: {user?.following_total ?? '-'}</Text>
-        <Text fontSize="md">created_at: {user?.created_at ?? '-'}</Text>
-        <Text fontSize="md">name: {user?.name ?? '-'}</Text>
-        <Text fontSize="md">company: {user?.company ?? '-'}</Text>
-        <Text fontSize="md">location: {user?.location ?? '-'}</Text>
-        <Text fontSize="md">email: {user?.email ?? '-'}</Text>
-        <Text fontSize="md">bio: {user?.bio ?? '-'}</Text>
+        <Box>
+          <Text fontSize="md">followers: {user?.followers_total ?? '-'}</Text>
+          <Text fontSize="md">following: {user?.following_total ?? '-'}</Text>
+          <Text fontSize="md">created_at: {user?.created_at ?? '-'}</Text>
+          <Text fontSize="md">company: {user?.company ?? '-'}</Text>
+          <Text fontSize="md">location: {user?.location ?? '-'}</Text>
+          <Text fontSize="md">
+            <EmailIcon />
+            email: {user?.email ?? '-'}
+          </Text>
+          <Text fontSize="md">bio: {user?.bio ?? '-'}</Text>
+        </Box>
       </Box>
     </Box>
   );
 });
 
-const StyledBlurredAvatarBackground = styled.div`
+const StyledBlurredBackCover = styled(Box)`
   position: absolute;
   z-index: -1;
   left: 0;
   top: 0;
   right: 0;
   bottom: 0;
+  border: 1px solid #000000;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  filter: blur(80px);
+  background-blend-mode: saturation;
+  transform: translate(-16px, 16px);
+  /* filter: blur(88px); */
+`;
+
+const StyledAvatar = styled(Image)`
+  position: absolute;
+  right: 0;
+  top: 0;
+  border-radius: 50%;
+  border: 1px solid #000000;
+`;
+
+const StyledUsername = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span:nth-child(1) {
+    font-size: 20px;
+  }
+  span:nth-child(2) {
+    font-size: 14px;
+    margin-left: 4px;
+  }
 `;
 
 export default UserCard;
