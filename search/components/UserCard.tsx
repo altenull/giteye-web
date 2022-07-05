dbb0b2285ea87b12db61cbdb710215f35e3f2dec
Heonyoung Kim <altenull@gmail.com>
2022-07-05 22:57:30 +0900

    WIP; New UserCard UI (2nd)

--- search/components/UserCard.tsx
+++ search/components/UserCard.tsx
@@ -1,7 +1,7 @@
 import { EmailIcon } from '@chakra-ui/icons';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Img, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
-import Image from 'next/image';
+import { differenceInDays, format } from 'date-fns';
 import React from 'react';
 import { useCustomRouter } from '../../hooks/useCustomRouter';
 import { SearchUser } from '../../pages/api/models/user.model';
@@ -15,51 +15,56 @@ const UserCard: React.FC<Props> = React.memo(({ searchUser }) => {
   const { user, isLoading, isError } = useGetUserAPI({ userName: searchUser.login });
   const { navigateToRepositories } = useCustomRouter();
 
+  const renderJoinedDate = (createdAt: string) => {
+    const createdDate: Date = new Date(createdAt);
+
+    return (
+      <Text textAlign="right" fontSize="md" fontWeight={500}>
+        Joined {format(createdDate, 'yyyy MMM dd')}(+{differenceInDays(new Date(), createdDate)})
+      </Text>
+    );
+  };
+
   return (
     <Box
       position="relative"
       cursor="pointer"
+      transform="translateX(10px)"
       marginBottom="40px"
       onClick={() => navigateToRepositories(searchUser.login)}
     >
-      <StyledBlurredBackCover
-        maxW="xl"
-        style={{
-          backgroundImage: `url(${searchUser.avatar_url})`,
-        }}
-      />
-
-      <Box
-        position="relative"
-        display="flex"
-        alignItems="center"
-        maxW="xl"
-        padding="16px"
-        border="1px solid #000000"
-        backgroundColor="white"
-      >
-        <StyledUsername>
-          <span>{searchUser.login}</span>
-          {typeof user?.name === 'string' && <span>({user.name})</span>}
-        </StyledUsername>
-
-        <StyledAvatar
+      <StyledBlurredBackCoverWrapper maxW="xl">
+        <StyledBlurredBackCoverImage
           style={{
-            position: 'absolute',
-            right: 0,
-            top: 0,
-            border: '1px solid #000000',
+            backgroundImage: `url(${searchUser.avatar_url})`,
           }}
-          src={searchUser.avatar_url}
-          alt={searchUser.login}
-          width={72}
-          height={72}
         />
+      </StyledBlurredBackCoverWrapper>
+
+      <Box maxW="xl" padding="32px 40px" border="1px solid #000000" backgroundColor="white">
+        <Box display="flex" justifyContent="space-between" alignItems="flex-start">
+          <StyledUsername>
+            <span>{searchUser.login}</span>
+            {typeof user?.name === 'string' && <span>({user.name})</span>}
+          </StyledUsername>
+
+          <Img
+            style={{
+              width: 56,
+              height: 56,
+              border: '1px solid black',
+              borderRadius: '50%',
+              marginTop: '8px',
+            }}
+            src={searchUser.avatar_url}
+            alt={searchUser.login}
+          />
+        </Box>
 
         <Box>
-          <Text fontSize="md">followers: {user?.followers_total ?? '-'}</Text>
-          <Text fontSize="md">following: {user?.following_total ?? '-'}</Text>
-          <Text fontSize="md">created_at: {user?.created_at ?? '-'}</Text>
+          <Text fontSize="md">
+            followers: {user?.followers_total ?? '-'} | following: {user?.following_total ?? '-'}
+          </Text>
           <Text fontSize="md">company: {user?.company ?? '-'}</Text>
           <Text fontSize="md">location: {user?.location ?? '-'}</Text>
           <Text fontSize="md">
@@ -67,13 +72,15 @@ const UserCard: React.FC<Props> = React.memo(({ searchUser }) => {
             email: {user?.email ?? '-'}
           </Text>
           <Text fontSize="md">bio: {user?.bio ?? '-'}</Text>
+
+          {typeof user?.created_at === 'string' && renderJoinedDate(user.created_at)}
         </Box>
       </Box>
     </Box>
   );
 });
 
-const StyledBlurredBackCover = styled(Box)`
+const StyledBlurredBackCoverWrapper = styled(Box)`
   position: absolute;
   z-index: -1;
   left: 0;
@@ -81,20 +88,17 @@ const StyledBlurredBackCover = styled(Box)`
   right: 0;
   bottom: 0;
   border: 1px solid #000000;
+  transform: translate(-20px, 20px);
+  overflow: hidden;
+`;
+
+const StyledBlurredBackCoverImage = styled(Box)`
+  height: 100%;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
   background-blend-mode: saturation;
-  transform: translate(-16px, 16px);
-  /* filter: blur(88px); */
-`;
-
-const StyledAvatar = styled(Image)`
-  position: absolute;
-  right: 0;
-  top: 0;
-  border-radius: 50%;
-  border: 1px solid #000000;
+  filter: blur(64px);
 `;
 
 const StyledUsername = styled.div`
@@ -103,10 +107,11 @@ const StyledUsername = styled.div`
   align-items: center;
 
   span:nth-child(1) {
-    font-size: 20px;
+    font-size: 26px;
+    font-weight: 500;
   }
   span:nth-child(2) {
-    font-size: 14px;
+    font-size: 16px;
     margin-left: 4px;
   }
 `;
