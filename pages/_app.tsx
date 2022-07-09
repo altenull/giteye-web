d55f87d16fca55f5078106cba633acfcc5088bd9
Heonyoung Kim <altenull@gmail.com>
2022-07-09 15:15:06 +0900

    feat: Set 'Pretendard' font as default font for Korean and English

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 import { SWRConfig } from 'swr';
 import '../styles/globals.css';
@@ -14,6 +15,13 @@ function MyApp({ Component, pageProps }: AppProps) {
             fetcher: (url: string) => fetch(url).then((res) => res.json()),
           }}
         >
+          <Head>
+            <link
+              rel="stylesheet"
+              as="style"
+              href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.4/dist/web/static/pretendard-dynamic-subset.css"
+            />
+          </Head>
           <Component {...pageProps} />
         </SWRConfig>
       </ChakraProvider>
